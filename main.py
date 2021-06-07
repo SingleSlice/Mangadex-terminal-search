df20cabd9808f1e6f65a1297cbe5e6663d0c3406
SingleSlice <castorina387@gmail.com>
2021-06-07 17:23:37 +0200

    noice graphics

--- main.py
+++ main.py
@@ -4,32 +4,26 @@ from _Getch import _Getch
 
 MD = MangaDexPy.MangaDex()
 Userinput = sys.argv#input()
-
 getch = _Getch()
 
-
-
 if len(Userinput) < 2:
-
     print("Please input a Manga name as an argument")
-else:
 
-    print("you searched : " + str(Userinput[1]) + "\n")
+else:
 
     for iteration ,manga in enumerate(MD.search("manga",{"title" : Userinput[1]}, 10)): # searches the manga
         print("["+str(iteration+1)+"]" + str(manga.title["en"])) #prints the manga list
 
-    print("\nwhich one?")
     mangaOn = int(getch())-1
     mangasc = MD.search("manga",{"title" : Userinput[1]}, 100)[mangaOn]
 
-    print("english title : " + str(mangasc.title["en"]))
-    print("tags : ", end="")
-    for tags in mangasc.tags:
+    print("\nenglish title : " + str(mangasc.title["en"]))
+    print("\n-- tags --")
 
-        print(tags.name["en"], end= "; ")
+    for tags in mangasc.tags:
+        print(tags.name["en"])
     
-    print("\n\n""description")
-    print("\n" + mangasc.desc["en"])
+    print("\n""-- description --")
+    print(mangasc.desc["en"])
 
 #print(MD.get_manga("ed996855-70de-449f-bba2-e8e24224c14d").title)

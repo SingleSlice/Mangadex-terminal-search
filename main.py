a6fd6a90cfeb2bd70dedd975cc8010c4a0b58105
SingleSlice <castorina387@gmail.com>
2021-06-08 20:08:26 +0200

    clear

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 import MangaDexPy, sys
+from os import system, name
 from _Getch import _Getch
+from _clear import _clear
 
 MD = MangaDexPy.MangaDex()
 Userinput = sys.argv#input()
 getch = _Getch()
+
+def _clear():
+  
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+  
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
 
 if len(Userinput) < 2:
     print("Please input a Manga name as an argument")
@@ -14,8 +26,9 @@ else:
         print("["+str(iteration+1)+"]" + str(manga.title["en"])) #prints the manga list
 
     mangaOn = int(getch())-1
+    _clear()
     mangasc = MD.search("manga",{"title" : Userinput[1]}, 100)[mangaOn]
-
+    
     print("\nenglish title : " + str(mangasc.title["en"]))
     print("\n-- tags --")
 
@@ -26,5 +39,6 @@ else:
     print(mangasc.desc["en"])
         
         
+
 
 #print(MD.get_manga("ed996855-70de-449f-bba2-e8e24224c14d").title)
